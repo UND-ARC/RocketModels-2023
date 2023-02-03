668ab87aa9278fffd294c1d62b0e110e04b8d6cc
Galbator <79435064+Galbator@users.noreply.github.com>
2023-02-03 16:05:19 -0600

    Better Variable Names and Optimization

--- NASA/Flight Data/Scale Model/pf2_to_csv/run.py
+++ NASA/Flight Data/Scale Model/pf2_to_csv/run.py
@@ -23,11 +23,11 @@ def getFieldNames (fieldNameLine):  # Returns a List of the Column Headers / Fie
     fieldNameLine = fieldNameLine[7:-1].strip().split(', ')
     return fieldNameLine
 
-def buildRow (j, headers, data):  # Returns a Dictionary Representing the Next Row to Print to the CSV File
+def buildRow (headers, data):  # Returns a Dictionary Representing the Next Row to Print to the CSV File
 
     row = {}; i = 0
     for head in headers:
-        row[head] = data[j][i]
+        row[head] = data[i]
         i += 1
     return row
 
@@ -39,29 +39,29 @@ def main ():
             test = fileName.strip().split('.')
             if (test[-1] != 'pf2'): raise TypeError
 
-            openFile = open(fileName)
+            selectedFile = open(fileName)
         except TypeError:
             print("\n\n\n\n\n -- Error: Invalid File Extension\n\n\n      - Valid Extensions: '.pf2'\n\n\n -- File Conversion Canceled\n\n\n -- Closing Application...\n\n\n\n\n\n")
             sleep(5); return
 
         print("\n" * 5); print(" -- File Selected: " + str(fileName) + "\n\n"); sleep(3)
 
-        fileName = fileName.strip().split('.')
-        fileName.append('csv')
-        fileName.pop(-2)
-        csvFileName = '.'.join(fileName)
+        temp = fileName.strip().split('.')
+        temp.append('csv')
+        temp.pop(-2)
+        csvFileName = '.'.join(temp)
 
         print(" -- CSV File Name: " + str(csvFileName) + "\n\n\n -- Converting...\n\n"); sleep(3)
 
         data = []; i = 0;
-        for line in openFile:
+        for line in selectedFile:
             if (line.startswith('Data:')):
                 fieldNameLine = line.strip();
             if (line[0].isnumeric()):
                 line = line.strip().split(', ')
                 data.append(line)
                 i += 1
-        openFile.close()
+        selectedFile.close()
 
         with open(csvFileName, 'w', newline='') as file:
             fieldNames = getFieldNames(fieldNameLine)
@@ -69,7 +69,7 @@ def main ():
 
             writer.writeheader()
             for j in range(0, i):
-                writer.writerow(buildRow(j, fieldNames, data))
+                writer.writerow(buildRow(fieldNames, data[j]))
         file.close()
         
         print(" -- Success: File Converted to '.csv'\n\n\n -- Closing Application...\n\n")
