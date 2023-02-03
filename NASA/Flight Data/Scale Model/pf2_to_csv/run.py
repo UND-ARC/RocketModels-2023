e22f05517fb8dc2aea5fff76e37e480e40ddf130
Galbator <79435064+Galbator@users.noreply.github.com>
2023-02-03 12:28:40 -0600

    Added Feedback; Fixed Imports; Better Performance

--- NASA/Flight Data/Scale Model/pf2_to_csv/run.py
+++ NASA/Flight Data/Scale Model/pf2_to_csv/run.py
@@ -1,7 +1,7 @@
 """
     Description: Converts a '.pf2' file to a '.csv'
 
-    Output: Output '.csv' File will be in the Same 'Spot' / 'Folder' at the Original File
+    Output: Output '.csv' File will be in the Same 'Folder' / 'Directory' as the Original '.pf2' File
 
     Dependencies: 
         FileUtils.py - Must be in the same 'Folder' / 'Directory' as this file
@@ -12,19 +12,16 @@
     Contributors:
         FileUtils.py - Tom Stokke
 """
-import csv
-import os
-import shutil
+from csv import DictWriter
+from time import sleep
 from FileUtils import selectOpenFile as SOF
 
-def getFieldNames (fieldNameLine):
-    # Returns a List of the Column Headers / Field Names of the CSV
+def getFieldNames (fieldNameLine):  # Returns a List of the Column Headers / Field Names of the CSV
 
     fieldNameLine = fieldNameLine[7:-1].strip().split(', ')
     return fieldNameLine
 
-def buildRow (j, headers, data):
-    # Returns a Dictionary Representing the Next Row to Print to the CSV File
+def buildRow (j, headers, data):  # Returns a Dictionary Representing the Next Row to Print to the CSV File
 
     row = {}; i = 0
     for head in headers:
@@ -37,9 +34,14 @@ def main ():
     try:
         openFile = open(fileName)
     except:
-        pass
+        print('\n' * 5)
+        print(" -- Error: File Conversion Canceled\n\n -- Closing Application...\n")
+        print('\n' * 5)
+        sleep(5); exit()
 
+    print("\n" * 5); print(" -- File Selected: " + str(fileName) + "\n\n"); sleep(3)
     csvFileName = fileName[:-4] + '.csv'
+    print(" -- CSV File Name: " + str(csvFileName) + "\n\n\n -- Converting...\n\n"); sleep(3)
 
     data = []; i = 0;
     for line in openFile:
@@ -49,17 +51,18 @@ def main ():
             line = line.strip().split(', ')
             data.append(line)
             i += 1
+    openFile.close()
 
     with open(csvFileName, 'w', newline='') as file:
         fieldNames = getFieldNames(fieldNameLine)
-        writer = csv.DictWriter(file, fieldnames=fieldNames)
+        writer = DictWriter(file, fieldnames=fieldNames)
 
         writer.writeheader()
         for j in range(0, i):
-            row = buildRow(j, fieldNames, data)
-            writer.writerow(row)
-
-    openFile.close()
+            writer.writerow(buildRow(j, fieldNames, data))
     file.close()
+    
+    print(" -- Success: File Converted to '.csv'\n\n\n -- Closing Application...\n\n")
+    sleep(5)
 
 main();
