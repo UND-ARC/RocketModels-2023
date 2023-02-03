c2b70cd9d206873c79e226e681af0030d4381593
Galbator <79435064+Galbator@users.noreply.github.com>
2023-02-03 00:21:01 -0600

    Works on all '.pf2' to '.csv' files

--- NASA/Flight Data/Scale Model/pf2_to_csv/run.py
+++ NASA/Flight Data/Scale Model/pf2_to_csv/run.py
@@ -1,21 +1,36 @@
 """
     Description: Converts a '.pf2' file to a '.csv'
 
-    Output: Output File will be in 'output' Folder / Directory inside 'pf2_to_csv'
+    Output: Output '.csv' File will be in the Same 'Spot' / 'Folder' at the Original File
 
     Dependencies: 
         FileUtils.py - Must be in the same 'Folder' / 'Directory' as this file
 
     Author: 
-        Mason Motschke
+        run.py - Mason Motschke
 
     Contributors:
-        FileUtil.py - Tom Stokke
+        FileUtils.py - Tom Stokke
 """
 import csv
 import os
 import shutil
 from FileUtils import selectOpenFile as SOF
+
+def getFieldNames (fieldNameLine):
+    # Returns a List of the Column Headers / Field Names of the CSV
+
+    fieldNameLine = fieldNameLine[7:-1].strip().split(', ')
+    return fieldNameLine
+
+def buildRow (j, headers, data):
+    # Returns a Dictionary Representing the Next Row to Print to the CSV File
+
+    row = {}; i = 0
+    for head in headers:
+        row[head] = data[j][i]
+        i += 1
+    return row
 
 def main ():
     fileName = SOF('Choose a File: ')
@@ -26,27 +41,25 @@ def main ():
 
     csvFileName = fileName[:-4] + '.csv'
 
-    data = []; i = 0
+    data = []; i = 0;
     for line in openFile:
+        if (line.startswith('Data:')):
+            fieldNameLine = line.strip();
         if (line[0].isnumeric()):
             line = line.strip().split(', ')
             data.append(line)
             i += 1
 
     with open(csvFileName, 'w', newline='') as file:
-        fieldNames = ['Time', 'Altitude', 'Velocity', 'Temperature (F)', 'Voltage']
+        fieldNames = getFieldNames(fieldNameLine)
         writer = csv.DictWriter(file, fieldnames=fieldNames)
 
         writer.writeheader()
         for j in range(0, i):
-            writer.writerow({'Time':data[j][0], 'Altitude':data[j][1], 'Velocity':data[j][2], 'Temperature (F)':data[j][3], 'Voltage':data[j][4]})
+            row = buildRow(j, fieldNames, data)
+            writer.writerow(row)
 
     openFile.close()
     file.close()
-
-    # Moves file to 'output' directory
-    os.chdir(os.getcwd() + '\\' + 'pf2_to_csv\\')
-    os.system('mkdir output')
-    shutil.move(csvFileName, os.getcwd() + '\\output\\')
 
 main();
